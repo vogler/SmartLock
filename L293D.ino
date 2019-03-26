4e1dce88d1b564bf3f530d0785bb28527fa628d0
Ralf Vogler <ralf.vogler@gmail.com>
2019-03-26 23:03:42 +0100

    power consumption of DOIT board and bare ESP32 on 2 AA batteries

--- L293D.ino
+++ L293D.ino
@@ -3,6 +3,12 @@
 // with 5V from USB for logic the L293D works fine, with 3.3V it only turned the motor in one direction...
 // used step-down from 9V to 5V for L293D logic and 2 AA (~3.2V) to power ESP32
 
+// power consumption on 3.2V from 2 AA:
+// - DOIT ESP32 DEVKIT V1:
+//   48.2 mA with motor running, 10.0 mA in deep sleep :(
+// - bare ESP32 on white PCB (used board FireBeetle-ESP32 in Arduino):
+//   36.8 mA w/o anything connected, 5.2 uA deep sleep (measured with Voltcraft VC275TRMS with 3.3V from Rigol DP832 b/c my ANENG AN8009 at home couldn't deal with the current on the uA plug despite saying 200mA max)
+
 const int en1 = 25;	// that is the pin that we use to control the motor's speed
 const int in1 = 32;	// this is the pin that we use to tell the motor to go forward
 const int in2 = 33;	// this is the pin that we use to tell the motor to go reverse
