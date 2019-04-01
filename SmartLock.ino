e507021c13fb078c1fd91e616dc3ff7f26b10018
Ralf Vogler <ralf.vogler@gmail.com>
2019-04-01 23:49:23 +0200

    notes on power consumption, played around with touch threshold

--- SmartLock.ino
+++ SmartLock.ino
@@ -1,19 +1,27 @@
 // 3D printed smart lock using:
 // - bare ESP32 (WROOM-32)
 //   - power via 2 AA batteries
-// - N20 geared motor driven by TB6612FNG I2C motor shield
+// - N20 geared motor driven by TB6612FNG I2C motor shield (D1 to GPIO22, D2 to GPIO21)
 //   - power via 9V block battery
 // see folder dev/ for more details
+
+// TB6612FNG shield's STM32F030 flashed with fixed firmware from https://hackaday.io/project/18439-motor-shield-reprogramming
+
+// power consumption (motor at 9 V, rest at ~3.2 V of 2 AA)
+//   motor: ~40 mA no load
+//   DOIT DEVKIT V1: 48.2 mA, 10 mA deep sleep
+//   bare ESP32: 36.8 mA, 5.2 uA deep sleep, with shield connected 16.9 uA?
+//   TB6612FNG I2C shield: 2.8 mA, 1.8 mA in standby?
 
 #include "WEMOS_Motor.h"
 
 Motor M1(0x30, _MOTOR_B, 1000);
 
-#define TOUCH_TH 10 // touch threshold, values usually 2-8 with finger depending on how hard the pinch
+#define TOUCH_TH 50 // touch threshold, values usually 2-12 with finger depending on how hard the pinch, on batteries somehow higher
 
 void setup()
 {
-  Serial.begin(250000);
+  Serial.begin(115200);
   Serial.println("setup");
 }
 
@@ -27,12 +35,14 @@ void loop()
   bool do_open  = touch_open && touch_open < TOUCH_TH;
   bool do_close = touch_close && touch_close < TOUCH_TH;
   uint8_t cmd;
-  if (do_open && do_close) cmd = _SHORT_BRAKE;
+  if (do_open && do_close) cmd = _STANDBY;
   else if (do_open) cmd = _CCW;
   else if (do_close) cmd = _CW;
-  else cmd = _STOP;
+  else cmd = _STOP; // _SHORT_BRAKE
   M1.setmotor(cmd);
 
-//  esp_sleep_enable_timer_wakeup(5000 * 1000);
-//  esp_deep_sleep_start();
+  if (cmd == _STANDBY) {
+    esp_sleep_enable_timer_wakeup(20 * 1000 * 1000);
+    esp_deep_sleep_start();
+  }
 }
