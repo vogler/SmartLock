dd02a4bacf59fce15ea4e4605963c56795af403b
Ralf Vogler <ralf.vogler@gmail.com>
2020-01-28 17:55:08 +0100

    use touchAttachInterrupt instead of unreliable touchRead

--- SmartLock.ino
+++ SmartLock.ino
@@ -38,6 +38,11 @@ void motor_standby(){
 #define TOUCH_TH 50 // touch threshold, values usually 2-12 with finger depending on how hard the pinch, on batteries somehow higher
 // with LOLIN D32 at pin 15 usually ~60, pin held to screw on the inside: ~33, when touching knob from outside: ~21
 
+bool touch1 = false;
+bool touch2 = false;
+void touch1_ISR(){ touch1 = true; }
+void touch2_ISR(){ touch2 = true; }
+
 void setup()
 {
   Serial.begin(115200);
@@ -46,25 +51,40 @@ void setup()
   pinMode(PIN_BIN1, OUTPUT);
   pinMode(PIN_BIN2, OUTPUT);
   pinMode(PIN_STBY, OUTPUT);
+
+  // use touch interrupts instead of touchRead in loop because it sometimes triggers without touch (maybe if reading too often?)
+  touchAttachInterrupt(T2, touch1_ISR, TOUCH_TH);
+  touchAttachInterrupt(T3, touch2_ISR, TOUCH_TH);
 }
 
 void loop()
 {
-  delay(100);
   // overview which pins are save to use: https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
   // pins falling below threshold without touching: 32, 33
-  const byte touch_open  = touchRead(2);
-  const byte touch_close = touchRead(15); // if the first read drops below threshold, the second one will go to 0 as well after some time
+  // const byte touch_open  = touchRead(2);
+  // const byte touch_close = touchRead(15); // if the first read drops below threshold, the second one will go to 0 as well after some time
   // sadly all touch pins report 0 every ~20 iterations without being touched, so we have to ignore 0
   // without touch the values are ~60-80, with touch 1-15
-  bool do_open  = touch_open && touch_open < TOUCH_TH;
-  bool do_close = touch_close && touch_close < TOUCH_TH;
-  if (do_open || do_close)
-    Serial.printf("touch: open %d, close %d\n", touch_open, touch_close);
-  uint8_t cmd;
+  // bool do_open  = touch_open && touch_open < TOUCH_TH;
+  // bool do_close = touch_close && touch_close < TOUCH_TH;
+  // if (do_open || do_close)
+    // Serial.printf("touch: open %d, close %d\n", touch_open, touch_close);
+
+  delay(100);
+  bool do_open = touch1;
+  bool do_close = touch2;
+  if(touch1){
+    touch1 = false;
+    Serial.println("Touch 1 detected");
+  }
+  if(touch2){
+    touch2 = false;
+    Serial.println("Touch 2 detected");
+  }
   if (do_open && do_close) {
     motor_standby();
     esp_sleep_enable_timer_wakeup(20 * 1000 * 1000);
+    Serial.println("Going to sleep for 20s because you told me to.");
     esp_deep_sleep_start();
   }
   else if (do_open) motor_CCW();
